6f67eb261d74b894a47f9b80ddccf298bb10eb62
Allen Barker <Allen.L.Barker@gmail.com>
2022-05-04 21:07:33 -0400

    get entry point working

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=["wheel"],
     python_requires=">=3.6",
     entry_points = {
-        "console_scripts": ["recdroidvid = recdroidvid.recdroid.py:process_command_line"]
+        "console_scripts": ["recdroidvid = recdroidvid.recdroidvid_main:main"]
         },
     url="https://github.com/abarker/recdroidvid",
 
